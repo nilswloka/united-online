88ff458c2dede2ba2ae7cd66b1ac58771778ce1f
Nils Wloka <mail@nilswloka.com>
2011-06-17 11:57:14 +0200

    Initial commit with empty Rails 3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnitedOnline::Application.config.secret_token = '3f6ee253bcd308e955c09b5ca0fdc9ebc29bd9ee17ee42c93304ef83d35cbba8374292a3a41713cfa3f1bb66326980c8a27b09c87594d78ecd784c2546e27eb3'
